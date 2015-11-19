d6dd9419fda2e8b2cc2a15a669f358c83b58ea8b
xuzhaocheng <xuzhaocheng@louis-macbook-pro.local>
2015-11-19 12:52:18 +0800

    Initial Commit

---
+++ CollectorNaivgation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectorNaivgation
+//
+//  Created by xuzhaocheng on 15/11/19.
+//  Copyright © 2015年 nmlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectorNaivgation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectorNaivgation
+//
+//  Created by xuzhaocheng on 15/11/19.
+//  Copyright © 2015年 nmlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
